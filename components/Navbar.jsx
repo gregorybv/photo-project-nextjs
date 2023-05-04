6872487ed6085fc3811096f9d0e8a301a91f528c
Gregory <gregory_berezin@outlook.com>
2023-05-04 21:30:57 +0300

    new padding

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       style={{ backgroundColor: `${color}` }}
       className='fixed left-0 top-0 w-full z-10 ease-in duration-300'
     >
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white '>
+      <div className='max-w-[1240px] m-auto flex justify-between items-center p-3 text-white '>
         <Link href='/'>
           <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl'>
             BG
