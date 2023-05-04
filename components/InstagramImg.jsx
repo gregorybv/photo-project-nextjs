739db0b4cbb8a7689009e6fbb6cbc5f0a0da6cab
Gregory <gregory_berezin@outlook.com>
2023-05-04 20:30:49 +0300

    new edits

--- components/InstagramImg.jsx
+++ components/InstagramImg.jsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
+import Image from "next/image"
+import React from "react"
+import { FaInstagram } from "react-icons/fa"
 
 const InstagramImg = ({ socialImg }) => {
   return (
@@ -13,12 +13,12 @@ const InstagramImg = ({ socialImg }) => {
       />
       {/* Overlay */}
       <div className='flex justify-center w-full h-full items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/50 group'>
-      <p className='text-gray-300 hidden group-hover:block'>
-        <FaInstagram size={30} className='z-10' />
-      </p>
+        <p className='text-gray-300 hidden group-hover:block'>
+          <FaInstagram size={30} className='z-10' />
+        </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default InstagramImg;
+export default InstagramImg
