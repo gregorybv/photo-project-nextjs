27a23fbf043e39aab8f0e184f634e6ce082d67c4
Gregory <gregory_berezin@outlook.com>
2023-05-05 19:38:23 +0300

    new edits

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Hero = ({ heading, message }) => {
+const Hero = ({ heading, message, text }) => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
       {/* Overlay */}
@@ -8,6 +8,7 @@ const Hero = ({ heading, message }) => {
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
+        <p>{text}</p>
         <button className='px-8 py-2 border hover:bg-white hover:text-black'>Book</button>
       </div>
     </div>
